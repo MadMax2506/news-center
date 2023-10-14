33bdf40a4973aa92724284538e0522ff7753ce66
Max Janorschke <programming@max-janorschke.de>
2023-10-14 17:07:57 +0200

    fix(navigation): :ambulance: variable to function

--- src/components/Navigation/Menu/MenuBody.tsx
+++ src/components/Navigation/Menu/MenuBody.tsx
@@ -1,12 +1,32 @@
+import { Dashboard as DashboardIcon, Newspaper as NewspaperIcon, WbSunny as WeatherIcon } from '@mui/icons-material';
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { paths } from '@routes';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import { MENU_ITEMS } from './menu.const';
+import { MenuItem } from './menu.types';
 
 type MenuBodyProps = {
   isMobile?: boolean;
 };
+
+const generateMenuItems = (): MenuItem[] => [
+  {
+    textKey: 'menu.dashboard',
+    icon: DashboardIcon,
+    path: paths.root.pattern,
+  },
+  {
+    textKey: 'menu.news',
+    icon: NewspaperIcon,
+    path: paths.news.pattern,
+  },
+  {
+    textKey: 'menu.weather',
+    icon: WeatherIcon,
+    path: paths.weather.pattern,
+  },
+];
 
 export const MenuBody: FC<MenuBodyProps> = ({ isMobile = false }): JSX.Element => {
   const { t } = useTranslation();
@@ -14,7 +34,7 @@ export const MenuBody: FC<MenuBodyProps> = ({ isMobile = false }): JSX.Element =
 
   return (
     <List sx={isMobile ? { my: 6 } : { mt: 2, mx: 3 }}>
-      {MENU_ITEMS.map(({ textKey, icon: Icon, path }) => (
+      {generateMenuItems().map(({ textKey, icon: Icon, path }) => (
         <ListItem key={textKey} disablePadding onClick={() => navigate(path)}>
           <ListItemButton>
             <ListItemIcon>
